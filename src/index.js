5a8a23709d00a890df10130fdb3e19d6e6a37fae
hellvita <seredaolga03@gmail.com>
2026-02-02 23:37:15 +0200

    add examples of the path module usage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const you = 'helvita';
+// автоматичне створення шляхів за допомогою модуля node:path
+import path from 'node:path';
 
-console.log('you: ', you);
+const pathExample = path.join('folder-1', 'file_4.txt');
+console.log('pathExample: ', pathExample);
+
+const pathToWorkDir = path.join(process.cwd());
+const pathToFile = path.join(pathToWorkDir, 'src', 'index.js');
+console.log('pathToFile: ', pathToFile);
+
+const pathParts = path.parse(pathToFile);
+console.log("pathParts: ", pathParts);
