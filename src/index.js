bcde4e9fe95786786d5d58658f27299f06db40a3
hellvita <seredaolga03@gmail.com>
2026-02-03 14:59:10 +0200

    add the examples of the fs module usage (read from the file)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// автоматичне створення шляхів за допомогою модуля node:path
+// ?? автоматичне створення шляхів за допомогою модуля node:path
 import path from 'node:path';
 
 const pathExample = path.join('folder-1', 'file_4.txt');
@@ -10,3 +10,32 @@ console.log('pathToFile: ', pathToFile);
 
 const pathParts = path.parse(pathToFile);
 console.log('pathParts: ', pathParts);
+
+// ?? робота з файлами та папками за допомогою модуля node:fs
+// ** синхронний fs
+import fs from 'node:fs';
+
+const file1 = path.join(pathToWorkDir, 'files', 'file_1.txt');
+
+// без кодування дургого аргумента,
+// що відповідає за кодування
+const buffer1 = fs.readFileSync(file1);
+console.log('buffer1: ', buffer1);
+
+// з кодуванням дургого аргумента,
+// що відповідає за кодування
+const file1Data = fs.readFileSync(file1, 'utf8');
+console.log('file1Data: ', file1Data);
+
+// ** асинхронний fs
+import fs from 'node:fs/promises';
+
+// без кодування дургого аргумента,
+// що відповідає за кодування
+const buffer1Async = await fs.readFile(file1);
+console.log('buffer1Async: ', buffer1Async);
+
+// з кодуванням дургого аргумента,
+// що відповідає за кодування
+const file1DataAsync = await fs.readFile(file1, 'utf8');
+console.log('file1DataAsync: ', file1DataAsync);
