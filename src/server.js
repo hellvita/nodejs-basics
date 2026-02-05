40ca145709c2372030a951027d09b026ae51463d
hellvita <seredaolga03@gmail.com>
2026-02-05 16:18:44 +0200

    add examples on how to work with dynamic params (express)

--- src/server.js
+++ src/server.js
@@ -16,6 +16,33 @@ app.get('/life', (req, res) => {
   res.status(200).json({ message: 'How you been doing, helvita?~☻' });
 });
 
+// список усіх станів ('http://localhost:3000/helvita/states')
+app.get('/helvita/states', (req, res) => {
+  res.status(200).json([
+    { id: 1, state: "i'm suuuuper!!!!" },
+    { id: 2, state: "i'm fine)" },
+  ]);
+});
+
+const getTempState = (id) => {
+  const numID = Number(id);
+  if (numID === 1) {
+    return { state: "i'm suuuuper!!!!" };
+  } else if (numID === 2) {
+    return { state: "i'm fine)" };
+  } else
+    return {
+      state: `i'm still fine, though there is no such an id as '${numID}'...`,
+    };
+};
+
+// конкретний стан за id ('http://localhost:3000/helvita/states/1')
+app.get('/helvita/states/:helvitaID', (req, res) => {
+  const { helvitaID } = req.params; // Параметри завжди приходять у вигляді рядків
+  const state = getTempState(helvitaID);
+  res.status(200).json({ id: helvitaID, ...state });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
