20c206bbbe666e500233f0a3b2e8500d46cb046e
hellvita <seredaolga03@gmail.com>
2026-02-07 15:57:27 +0200

    fix the avgMark field name, add error messages for the updateStudentSchema

--- src/validations/studentsValidation.js
+++ src/validations/studentsValidation.js
@@ -26,41 +26,122 @@ export const studentIdParamSchema = {
 // під час створення нового студента
 export const createStudentSchema = {
   [Segments.BODY]: Joi.object({
-    name: Joi.string().min(3).max(30).required().messages({
-      'string.base': 'Name must be a string',
-      'string.min': 'Name should have at least {#limit} characters',
-      'string.max': 'Name should have at most {#limit} characters',
-      'any.required': 'Name is required',
-    }),
-    age: Joi.number().integer().min(12).max(65).required().messages({
-      'number.base': 'Age must be a number',
-      'number.min': 'Age must be at least {#limit}',
-      'number.max': 'Age must be at most {#limit}',
-      'any.required': 'Age is required',
-    }),
-    gender: Joi.string().valid('male', 'female', 'other').required().messages({
-      'any.only': 'Gender must be one of: male, female, or other',
-      'any.required': 'Gender is required',
-    }),
-    avgMart: Joi.number().min(2).max(12).required().messages({
-      'number.base': 'Average mark must be a number',
-      'number.min': 'Average mark must be at least {#limit}',
-      'number.max': 'Average mark must be at most {#limit}',
-      'any.required': 'Average mark is required',
-    }),
-    onDuty: Joi.boolean().messages({
-      'boolean.base': 'onDuty must be a boolean value',
-    }),
+    name: Joi.string()
+      .min(3)
+      .max(30)
+      .required()
+      .messages(studentErrorMessages('name', true)),
+    age: Joi.number()
+      .integer()
+      .min(12)
+      .max(65)
+      .required()
+      .messages(studentErrorMessages('age', true)),
+    gender: Joi.string()
+      .valid('male', 'female', 'other')
+      .required()
+      .messages(studentErrorMessages('gender', true)),
+    avgMark: Joi.number()
+      .min(2)
+      .max(12)
+      .required()
+      .messages(studentErrorMessages('avgMark', true)),
+    onDuty: Joi.boolean()
+      .required()
+      .messages(studentErrorMessages('onDuty', true)),
   }),
 };
 
 export const updateStudentSchema = {
   ...studentIdParamSchema,
   [Segments.BODY]: Joi.object({
-    name: Joi.string().min(3).max(30),
-    age: Joi.number().integer().min(12).max(65),
-    gender: Joi.string().valid('male', 'female', 'other'),
-    avgMart: Joi.number().min(2).max(12),
-    onDuty: Joi.boolean(),
+    name: Joi.string().min(3).max(30).messages(studentErrorMessages('name')),
+    age: Joi.number()
+      .integer()
+      .min(12)
+      .max(65)
+      .messages(studentErrorMessages('age')),
+    gender: Joi.string()
+      .valid('male', 'female', 'other')
+      .messages(studentErrorMessages('gender')),
+    avgMark: Joi.number()
+      .min(2)
+      .max(12)
+      .messages(studentErrorMessages('avgMark')),
+    onDuty: Joi.boolean().messages(studentErrorMessages('onDuty')),
   }).min(1),
 };
+
+function studentErrorMessages(value, valueIsRequired = false) {
+  switch (value) {
+    case 'name': {
+      const defaultMessage = {
+        'string.base': 'Name must be a string',
+        'string.min': 'Name should have at least {#limit} characters',
+        'string.max': 'Name should have at most {#limit} characters',
+      };
+      const requiredMessage = { 'any.required': 'Name is required' };
+
+      const finalMessage = valueIsRequired
+        ? { ...defaultMessage, ...requiredMessage }
+        : defaultMessage;
+
+      return finalMessage;
+    }
+    case 'age': {
+      const defaultMessage = {
+        'number.base': 'Age must be a number',
+        'number.min': 'Age must be at least {#limit}',
+        'number.max': 'Age must be at most {#limit}',
+      };
+      const requiredMessage = { 'any.required': 'Age is required' };
+
+      const finalMessage = valueIsRequired
+        ? { ...defaultMessage, ...requiredMessage }
+        : defaultMessage;
+
+      return finalMessage;
+    }
+    case 'gender': {
+      const defaultMessage = {
+        'any.only': 'Gender must be one of: male, female, or other',
+      };
+      const requiredMessage = { 'any.required': 'Gender is required' };
+
+      const finalMessage = valueIsRequired
+        ? { ...defaultMessage, ...requiredMessage }
+        : defaultMessage;
+
+      return finalMessage;
+    }
+    case 'avgMark': {
+      const defaultMessage = {
+        'number.base': 'Average mark must be a number',
+        'number.min': 'Average mark must be at least {#limit}',
+        'number.max': 'Average mark must be at most {#limit}',
+      };
+      const requiredMessage = { 'any.required': 'Average mark is required' };
+
+      const finalMessage = valueIsRequired
+        ? { ...defaultMessage, ...requiredMessage }
+        : defaultMessage;
+
+      return finalMessage;
+    }
+    case 'onDuty': {
+      const defaultMessage = {
+        'boolean.base': 'onDuty must be a boolean value',
+      };
+      const requiredMessage = { 'any.required': 'onDuty is required' };
+
+      const finalMessage = valueIsRequired
+        ? { ...defaultMessage, ...requiredMessage }
+        : defaultMessage;
+
+      return finalMessage;
+    }
+    default: {
+      return {};
+    }
+  }
+}
