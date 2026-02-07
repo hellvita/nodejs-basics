50f4cc9b9c3af094be47e70c51d88af062d2b1fd
hellvita <seredaolga03@gmail.com>
2026-02-08 00:20:43 +0200

    add fields for filtration in the students validation schema

--- src/validations/studentsValidation.js
+++ src/validations/studentsValidation.js
@@ -10,10 +10,13 @@ import { isValidObjectId } from 'mongoose';
 **
 */
 
+// TODO: add messages
 export const getStudentsSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
+    gender: Joi.string().valid('male', 'female', 'other'),
+    minAvgMark: Joi.number().positive(),
   }),
 };
 
