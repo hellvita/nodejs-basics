3677445162eb7fd9353ff064a6ec82f2dcd509f4
hellvita <seredaolga03@gmail.com>
2026-02-09 16:47:01 +0200

    add the validation schema for the login user object

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,4 +1,5 @@
 import { Joi, Segments } from 'celebrate';
+import { Schema } from 'mongoose';
 
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
@@ -6,3 +7,10 @@ export const registerUserSchema = {
     password: Joi.string().min(6).required(),
   }),
 };
+
+export const loginUserSchema = new Schema({
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required().lowercase(),
+    password: Joi.string().required(),
+  }),
+});
