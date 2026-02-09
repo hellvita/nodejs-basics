db2418f5c71bc32d6ff169e18c31f0eb7382afe6
hellvita <seredaolga03@gmail.com>
2026-02-09 21:21:54 +0200

    create and update session in the authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { User } from '../models/user.js';
+import { createSession } from '../services/auth.js';
+import { Session } from '../models/session.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -13,6 +15,8 @@ export const registerUser = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = await User.create({ email, password: hashedPassword });
+
+  await createSession(newUser._id);
 
   res.status(201).json(newUser);
 };
@@ -29,6 +33,9 @@ export const loginUser = async (req, res) => {
   if (!isValidPassword) {
     throw createHttpError(401, 'Invalid email or password');
   }
+
+  await Session.deleteOne({ userId: user._id });
+  await createSession(user._id);
 
   res.status(200).json(user);
 };
