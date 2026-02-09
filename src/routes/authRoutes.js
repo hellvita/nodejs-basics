144595b063037a05241ff95e7a156727075688b1
hellvita <seredaolga03@gmail.com>
2026-02-09 21:58:57 +0200

    add the route for the logoutUser method

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
@@ -10,5 +14,6 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
 
 export default router;
