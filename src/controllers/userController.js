d1b887568d6b1a8b356581c7d434fa9d22c43ef2
hellvita <seredaolga03@gmail.com>
2026-02-12 00:29:34 +0200

    add the file object to the updateUserAvatar method

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,13 @@
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+
 export const updateUserAvatar = async (req, res) => {
+  const { file } = req;
+  if (!file) {
+    throw createHttpError(400, 'No file has been loaded');
+  }
+
+  console.log(file);
+
   res.status(200).json({ url: '' });
 };
