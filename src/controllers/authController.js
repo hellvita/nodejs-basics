ccf4c9102b1421e356a29663054c0d6646973e77
hellvita <seredaolga03@gmail.com>
2026-02-11 23:14:03 +0200

    add the resetPassword method

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -139,3 +139,28 @@ export const requestResetEmail = async (req, res, next) => {
     .status(200)
     .json({ message: 'If this email exists, a reset link has been sent' });
 };
+
+export const resetPassword = async (req, res) => {
+  const { password, token } = req.body;
+
+  let payload = null;
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    throw createHttpError(401, 'Invalid or expired token');
+  }
+
+  const user = await User.findOne({ _id: payload.sub, email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await User.updateOne({ _id: user._id }, { password: hashedPassword });
+
+  await Session.deleteMany({ userId: user._id });
+
+  res
+    .status(200)
+    .json({ message: 'Password reset successfully. Please log in again.' });
+};
