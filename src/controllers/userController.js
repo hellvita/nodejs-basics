25fcc96b1dd72b64ecec7104a3bea6697c690ac7
hellvita <seredaolga03@gmail.com>
2026-02-12 16:12:31 +0200

    update the updateUserAvatar method (use file upload utility and update the user)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const updateUserAvatar = async (req, res) => {
   const { file } = req;
@@ -7,7 +8,13 @@ export const updateUserAvatar = async (req, res) => {
     throw createHttpError(400, 'No file has been loaded');
   }
 
-  console.log(file);
+  const result = await saveFileToCloudinary(req.file.buffer, req.user._id);
 
-  res.status(200).json({ url: '' });
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: result.secure_url },
+    { new: true },
+  );
+
+  res.status(200).json({ url: user.avatar });
 };
